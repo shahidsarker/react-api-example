2fa548e81991f54185a5240758f1dc1171f6f2c8
Shahid <shahid.sarker@gmail.com>
2020-06-04 10:48:08 -0400

    Pokemon component pulls hard-coded data from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Pokemon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { pokemon: { name: "" } };
+  }
+
+  componentDidMount() {
+    axios.get("https://pokeapi.co/api/v2/pokemon/ditto").then((response) => {
+      const data = response.data;
+      const pokemon = {
+        name: data.name,
+        imageUrl: data.sprites.front_default,
+      };
+      this.setState({ pokemon });
+    });
+  }
+
+  render() {
+    return (
+      <div className="pokemon">
+        <img src={this.state.pokemon.imageUrl} />
+        <ul>
+          <li>Pokemon name: {this.state.pokemon.name} </li>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ReactJS with APIs</h1>
+        <Pokemon />
+      </div>
+    );
+  }
 }
 
 export default App;
