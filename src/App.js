259ec46a368561b31af815251a11b87d3a77f425
Shahid <shahid.sarker@gmail.com>
2020-06-04 10:58:30 -0400

    Pass props to Pokemon component

--- src/App.js
+++ src/App.js
@@ -5,27 +5,33 @@ import axios from "axios";
 class Pokemon extends Component {
   constructor(props) {
     super(props);
-    this.state = { pokemon: { name: "" } };
+    this.state = { pokemon: {} };
   }
 
+  // pokemonName = ;
+
   componentDidMount() {
-    axios.get("https://pokeapi.co/api/v2/pokemon/ditto").then((response) => {
-      const data = response.data;
-      const pokemon = {
-        name: data.name,
-        imageUrl: data.sprites.front_default,
-      };
-      this.setState({ pokemon });
-    });
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/" + this.props.name)
+      .then((response) => {
+        const data = response.data;
+
+        const newPokemonObj = {
+          name: data.name,
+          imageUrl: data.sprites.front_default,
+        };
+
+        this.setState({ pokemon: newPokemonObj });
+      })
+      .catch((err) => console.log(err));
   }
 
   render() {
     return (
       <div className="pokemon">
-        <img src={this.state.pokemon.imageUrl} />
+        <img src={this.state.pokemon.imageUrl} alt={this.state.pokemon.name} />
         <ul>
           <li>Pokemon name: {this.state.pokemon.name} </li>
-          <li></li>
         </ul>
       </div>
     );
@@ -37,7 +43,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>ReactJS with APIs</h1>
-        <Pokemon />
+        <Pokemon name="ditto" />
+        <Pokemon name="pikachu" />
+        <Pokemon name="mewtwo" />
+        <Pokemon name="shahid" />
       </div>
     );
   }
