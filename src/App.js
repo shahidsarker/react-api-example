8f625257e3bcc6fdf1b0576584127a62b791c072
Shahid <shahid.sarker@gmail.com>
2020-06-04 11:07:53 -0400

    Extract Pokemon component from App component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
+import Pokemon from "./components/Pokemon";
 import axios from "axios";
-
-class Pokemon extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { pokemon: null };
-  }
-
-  componentDidMount() {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/" + this.props.name)
-      .then((response) => {
-        const data = response.data;
-
-        const newPokemonObj = {
-          name: data.name,
-          imageUrl: data.sprites.front_default,
-        };
-
-        this.setState({ pokemon: newPokemonObj });
-      })
-      .catch((err) => console.log(err));
-  }
-
-  render() {
-    let display;
-    if (!this.state.pokemon) {
-      display = <p>Loading...</p>;
-    } else {
-      display = (
-        <>
-          <img
-            src={this.state.pokemon.imageUrl}
-            alt={this.state.pokemon.name}
-          />
-          <ul>
-            <li>{this.state.pokemon.name} </li>
-          </ul>
-        </>
-      );
-    }
-
-    return <div className="pokemon">{display}</div>;
-  }
-}
 
 class App extends Component {
   render() {
