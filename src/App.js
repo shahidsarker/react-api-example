7cd0daa2ca0d76dcb8e4faf06babd3f0abfb8059
Shahid <shahid.sarker@gmail.com>
2020-06-05 10:27:26 -0400

    Class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import Dictionary from "./components/Dictionary";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>ReactJS with APIs</h1>
-        <Dictionary word="test" />
-      </div>
-    );
-  }
-}
+const App = (props) => {
+  return (
+    <div className="App">
+      <h1>ReactJS with APIs</h1>
+      <Dictionary word="test" />
+    </div>
+  );
+};
 
 export default App;
